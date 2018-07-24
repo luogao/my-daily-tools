0aa413aea9712ba1c5c9e95b170af1ebc1df815d
罗高 <luogao_lg@sina.com>
2018-07-24 22:46:53 +0800

    merge home.js

--- pages/home/home.js
+++ pages/home/home.js
@@ -101,17 +101,9 @@ Page({
   onShareAppMessage: function () {
 
   },
-<<<<<<< HEAD
   goPage(e) {
-    console.log(e)
     const { page } = e.currentTarget.dataset
     const _url = `../${page}/${page}`
-    console.log(_url)
-=======
-  goPage(e){
-    
-    const _url = `../${url}/${url}`
->>>>>>> 12ddefb8621a2f19806ae7710d05ad14c75598c7
     wx.navigateTo({
       url: _url
     })
