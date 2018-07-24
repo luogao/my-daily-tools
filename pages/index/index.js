832cdeb3cfa36c5b053b91aa356536356557cc3d
罗高 <luogao_lg@sina.com>
2018-07-24 22:44:55 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -28,8 +28,8 @@ Page({
     })
   },
   fetchData(cb) {
-    let fullData = wx.getStorageSync('todolist') || []
-    let processedData = fullData.map(el => {
+    const fullData = wx.getStorageSync('todolist') || []
+    const processedData = fullData.map(el => {
       el.createAt = util.formatTime(new Date(el.createAt))
       el.finishAt = el.finishAt ? util.formatTime(new Date(el.finishAt)) : null
       el.deadline = el.deadline ? el.deadline.replace(/-/g, '/') : null
@@ -60,7 +60,16 @@ Page({
   //     wx.stopPullDownRefresh() //停止下拉刷新
   //   })
   // },
+  onReady() {
+    wx.showLoading({
+      title: '加载中...'
+    })
+    this.fetchData()
+  },
   onShow() {
+    wx.showLoading({
+      title: '加载中...'
+    })
     this.fetchData()
   },
   getLocation() {
@@ -88,45 +97,6 @@ Page({
     if (user) return true
     return false
   },
-  onLoad: function () {
-    
-    
-
-    // if (app.globalData.userInfo) {
-    //   this.setData({
-    //     userInfo: app.globalData.userInfo,
-    //     hasUserInfo: true
-    //   })
-    // } else if (this.data.canIUse) {
-    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-    //   // 所以此处加入 callback 以防止这种情况
-    //   app.userInfoReadyCallback = res => {
-    //     this.setData({
-    //       userInfo: res.userInfo,
-    //       hasUserInfo: true
-    //     })
-    //   }
-    // } else {
-    //   // 在没有 open-type=getUserInfo 版本的兼容处理
-    //   wx.getUserInfo({
-    //     success: res => {
-    //       app.globalData.userInfo = res.userInfo
-    //       this.setData({
-    //         userInfo: res.userInfo,
-    //         hasUserInfo: true
-    //       })
-    //     }
-    //   })
-    // }
-  },
-  // getUserInfo: function (e) {
-  //   console.log(e)
-  //   app.globalData.userInfo = e.detail.userInfo
-  //   this.setData({
-  //     userInfo: e.detail.userInfo,
-  //     hasUserInfo: true
-  //   })
-  // },
   createNew() {
     wx.navigateTo({
       url: '../details/details?id=-1'
